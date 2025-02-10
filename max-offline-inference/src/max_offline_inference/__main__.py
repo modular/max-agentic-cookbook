7631e0ee54fd42a0adab05372bc4ec84e89ed2ff
Bill Welense <billw@modular.com>
2025-02-10 13:03:06 -0600

    Initial commit of max-offline-inference

---
+++ max-offline-inference/src/max_offline_inference/__main__.py
@@ -0,0 +1,11 @@
+from max.entrypoints import LLM
+from max.pipelines import PipelineConfig
+from max.pipelines.architectures import register_all_models
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
