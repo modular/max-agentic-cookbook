e681b05fd9c1c809b38626f1423e96d181d4a638
Bill Welense <billw@modular.com>
2025-05-19 10:38:06 -0500

    Add demo FastMCP server

--- max-mcp-openai-agents-sdk/tasks.py
+++ max-mcp-openai-agents-sdk/tasks.py
@@ -1,5 +1,8 @@
+from fastmcp import FastMCP
 from honcho.manager import Manager
 from invoke import task, Context
+from pydantic import Field
+from typing import Annotated
 
 
 @task
@@ -10,14 +13,39 @@ def max(c: Context):
 
 
 @task
+def mcp(_c: Context):
+    mcp = FastMCP("Demo MCP Server")
+
+    @mcp.tool()
+    def count_characters(
+        character: Annotated[
+            str, Field(description="Character to count the occurrences of")
+        ],
+        string: Annotated[
+            str, Field(description="String within which to search for the character")
+        ],
+    ) -> int:
+        """Counts the occurrences of a character within a string"""
+        return string.count(character)
+
+    mcp.run(
+        transport="streamable-http",
+        host="127.0.0.1",
+        port=8001,
+        log_level="debug",
+    )
+
+
+@task
 def ui(_c: Context):
     while True:
         pass
 
 
 @task
-def serve(_c: Context):
+def app(_c: Context):
     m = Manager()
     m.add_process("max", "invoke max", quiet=False)
+    m.add_process("mcp", "invoke mcp", quiet=False)
     m.add_process("ui", "invoke ui", quiet=False)
     m.loop()
