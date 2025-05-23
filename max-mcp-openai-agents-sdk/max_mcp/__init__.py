e339f30cafab3cb4d9a42f451c5738f3545387a8
Bill Welense <billw@modular.com>
2025-05-23 10:12:51 -0500

    Improve error handling

--- max-mcp-openai-agents-sdk/max_mcp/__init__.py
+++ max-mcp-openai-agents-sdk/max_mcp/__init__.py
@@ -1,44 +1,48 @@
 from fastapi import FastAPI, HTTPException
-from fastmcp import Client as MCPClient
-from openai import OpenAI
+from returns.result import Success, Failure
 
 from .agent import (
-    ChatMessage,
-    ChatSession,
+    start_session,
     discover_tools,
     send_message,
     call_tool,
 )
 from .models import CountRequest, CountResult
 
-openai_client = OpenAI(base_url="http://127.0.0.1:8001/v1", api_key="EMPTY")
-mcp_client = MCPClient("http://127.0.0.1:8002/mcp")
-model = "meta-llama/Llama-3.2-1B-Instruct"
 
 app = FastAPI()
 
 
 @app.post("/count")
-async def handle_count(request: CountRequest) -> CountResult:
-    message = ChatMessage(role="user", content=request.query)
+async def count_api(request: CountRequest) -> CountResult:
+    match await start_session(request.query):
+        case Success(value):
+            session = value
+        case Failure(error):
+            raise HTTPException(status_code=400, detail=error)
+
+    match await discover_tools(session):
+        case Success(value):
+            session = value
+        case Failure(error):
+            raise HTTPException(status_code=400, detail=error)
+
+    match await send_message(session):
+        case Success(value):
+            session = value
+        case Failure(error):
+            raise HTTPException(status_code=400, detail=error)
+
+    match await call_tool(session):
+        case Success(value):
+            session = value
+        case Failure(error):
+            raise HTTPException(status_code=400, detail=error)
+
     try:
-        async with mcp_client:
-            session = ChatSession(
-                openai_client=openai_client,
-                mcp_client=mcp_client,
-                model=model,
-                messages=[message],
-            )
-
-            session = await discover_tools(session)
-            session = await send_message(session)
-            response_message = session.messages[-1]
-
-            if response_message.tool_call is not None:
-                session = await call_tool(session)
-                response_message = session.messages[-1]
-
-            return response_message.content
-
+        last_message = session.messages[-1]
+        response = CountResult.model_validate_json(last_message.content)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=f"/count failed: {e}")
+
+    return response
--- max-mcp-openai-agents-sdk/max_mcp/agent.py
+++ max-mcp-openai-agents-sdk/max_mcp/agent.py
@@ -1,55 +1,82 @@
 import json
+from typing import TypeAlias
+
+from fastmcp import Client as MCPClient
+from openai import OpenAI
+from returns.result import Result, Success, Failure
+
 from .models import ChatMessage, ChatSession, ToolCall
 
 
-async def discover_tools(session: ChatSession) -> ChatSession:
+openai_client = OpenAI(base_url="http://127.0.0.1:8001/v1", api_key="EMPTY")
+mcp_client = MCPClient("http://127.0.0.1:8002/mcp")
+model = "meta-llama/Llama-3.2-1B-Instruct"
+
+ChatSessionResult: TypeAlias = Result["ChatSession", str]
+
+async def start_session(query: str) -> ChatSessionResult:
     try:
-        mcp_tools = await session.mcp_client.list_tools()
-        tools = []
-
-        for tool in mcp_tools:
-            if (
-                (name := tool.name)
-                and (description := tool.description)
-                and (parameters := tool.inputSchema)
-            ):
-                formatted_tool = {
-                    "type": "function",
-                    "function": {
-                        "name": name,
-                        "description": description,
-                        "parameters": parameters,
-                    },
-                }
-                tools.append(formatted_tool)
-
-        session.tools = tools
-        return session
-
-    except Exception as e:
-        print(f"discover_tools failed - {e}")
-        raise e
-
-
-async def call_tool(session: ChatSession) -> ChatSession:
-    try:
-        last_message = session.messages[-1]
-        if (tool_call := last_message.tool_call) and (arguments := tool_call.arguments):
-            result = await session.mcp_client.call_tool(tool_call.name, arguments)
-            message = ChatMessage(
-                role="tool",
-                tool_call_id=last_message.tool_call_id,
-                content=result[0].text,
+        message = ChatMessage(role="user", content=query)
+        async with mcp_client:
+            session = ChatSession(
+                openai_client=openai_client,
+                mcp_client=mcp_client,
+                model=model,
+                messages=[message],
             )
-            session.messages.append(message)
-        return session
+        return Success(session)
+    except Exception as e:
+        handle_failure(e, "Starting session")
+
+
+async def discover_tools(session: ChatSession) -> ChatSessionResult:
+    try:
+        async with session.mcp_client:
+            mcp_tools = await session.mcp_client.list_tools()
+            tools = []
+
+            for tool in mcp_tools:
+                if (
+                    (name := tool.name)
+                    and (description := tool.description)
+                    and (parameters := tool.inputSchema)
+                ):
+                    formatted_tool = {
+                        "type": "function",
+                        "function": {
+                            "name": name,
+                            "description": description,
+                            "parameters": parameters,
+                        },
+                    }
+                    tools.append(formatted_tool)
+
+            session.tools = tools
+            return Success(session)
 
     except Exception as e:
-        print(f"call_tool failed - {e}")
-        raise e
+        handle_failure(e, "Discovering tools")
 
 
-async def send_message(session: ChatSession) -> ChatSession:
+async def call_tool(session: ChatSession) -> ChatSessionResult:
+    try:
+        async with session.mcp_client:
+            last_message = session.messages[-1]
+            if (tool_call := last_message.tool_call) and (arguments := tool_call.arguments):
+                result = await session.mcp_client.call_tool(tool_call.name, arguments)
+                message = ChatMessage(
+                    role="tool",
+                    tool_call_id=last_message.tool_call_id,
+                    content=result[0].text,
+                )
+                session.messages.append(message)
+            return Success(session)
+
+    except Exception as e:
+        handle_failure(e, "Calling tool")
+
+
+async def send_message(session: ChatSession) -> ChatSessionResult:
     try:
         messages = [
             {"role": m.role, "content": m.content, "tool_call_id": m.tool_call_id}
@@ -65,7 +92,6 @@ async def send_message(session: ChatSession) -> ChatSession:
         if response := response.choices[0].message:
             message = ChatMessage(role="assistant", content=response.content)
 
-            # TODO: Support more than one tool call
             if (
                 response.tool_calls
                 and (tool := response.tool_calls[0].function)
@@ -77,8 +103,13 @@ async def send_message(session: ChatSession) -> ChatSession:
 
             session.messages.append(message)
 
-        return session
+        return Success(session)
 
     except Exception as e:
-        print(f"send_message failed - {e}")
-        raise e
+        handle_failure(e, "Sending message")
+    
+
+def handle_failure(exception: Exception, what_failed: str) -> Failure:
+    message = f"{what_failed} failed: {exception}"
+    print(message)
+    return Failure(message)
