d7c38f9b7e4bebde9840915518858bf377e962a2
Bill Welense <billw@modular.com>
2025-09-23 13:45:06 -0500

    Handle missing recipes gracefully in RootLayout

--- genai-cookbook/app/recipes/layout.tsx
+++ genai-cookbook/app/recipes/layout.tsx
@@ -1,11 +1,16 @@
+import { BodyText } from '@/components/BodyText'
 import CookbookShell from '@/components/cookbook-shell'
 import recipeStore from '@/store/RecipeStore'
+import { Flex } from '@mantine/core'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     const recipes = recipeStore.getAll()
-    if (!recipes) {
-        throw new Error('No Recipes Loaded')
-    }
 
-    return <CookbookShell recipes={recipes}>{children}</CookbookShell>
+    return recipes ? (
+        <CookbookShell recipes={recipes}>{children}</CookbookShell>
+    ) : (
+        <Flex dir="column" justify="center" align="center" h="100vh">
+            <BodyText size="md">No Recipes Found</BodyText>
+        </Flex>
+    )
 }
